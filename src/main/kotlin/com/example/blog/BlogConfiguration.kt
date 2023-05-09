aab11a5033f9a33e398826f063ff7ffe36461230
zxisatree <62459441+zxisatree@users.noreply.github.com>
2023-05-09 19:20:23 +0800

    Use kotlin_blog psql database

--- src/main/kotlin/com/example/blog/BlogConfiguration.kt
+++ src/main/kotlin/com/example/blog/BlogConfiguration.kt
@@ -7,22 +7,23 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class BlogConfiguration {
 
-    @Bean
-    fun databaseInitializer(userRepository: UserRepository,
-                            articleRepository: ArticleRepository) = ApplicationRunner {
-
-        val johnDoe = userRepository.save(User("johnDoe", "John", "Doe"))
-        articleRepository.save(Article(
-            title = "Lorem",
-            headline = "Lorem",
-            content = "dolor sit amet",
-            author = johnDoe
-        ))
-        articleRepository.save(Article(
-            title = "Ipsum",
-            headline = "Ipsum",
-            content = "dolor sit amet",
-            author = johnDoe
-        ))
-    }
+//    For H2 databases
+//    @Bean
+//    fun databaseInitializer(userRepository: UserRepository,
+//                            articleRepository: ArticleRepository) = ApplicationRunner {
+//
+//        val johnDoe = userRepository.save(User("johnDoe", "John", "Doe"))
+//        articleRepository.save(Article(
+//            title = "Lorem",
+//            headline = "Lorem",
+//            content = "dolor sit amet",
+//            author = johnDoe
+//        ))
+//        articleRepository.save(Article(
+//            title = "Ipsum",
+//            headline = "Ipsum",
+//            content = "dolor sit amet",
+//            author = johnDoe
+//        ))
+//    }
 }
