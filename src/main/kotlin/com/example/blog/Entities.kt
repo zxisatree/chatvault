28e9e31acc449910947215ad9e9b8c8361495f28
zxisatree <62459441+zxisatree@users.noreply.github.com>
2023-05-09 09:59:06 +0800

    Implement articles
    App can't be started yet

---
+++ src/main/kotlin/com/example/blog/Entities.kt
@@ -0,0 +1,22 @@
+package com.example.blog
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class Article(
+    var title: String,
+    var headline: String,
+    var content: String,
+    @ManyToOne var author: User,
+    var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue var id: Long? = null)
+
+@Entity
+class User(
+    var login: String,
+    var firstname: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null)
